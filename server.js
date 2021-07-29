2b1e647de275d12ea0c7fd1596b55f5e6d151470
Emmy-Iraturinze <iraturinzeemmyy@gmail.com>
2021-07-29 15:50:46 +0200

    fbuif

--- server.js
+++ server.js
@@ -6,11 +6,11 @@
     const app = express();
 
     // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/todo'));
+    app.use(express.static(__dirname + '/dist/ira2-app'));
 
     app.get('/*', function(req,res) {
         
-    res.sendFile(path.join(__dirname+'/dist/todo/index.ejs'));
+    res.sendFile(path.join(__dirname+'/dist/ira2-app/index.ejs'));
     });
 
     // Start the app by listening on the default Heroku port
